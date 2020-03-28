8b6332672df346dd9bcc75fc3ede2c7587169eb0
Daniel Salinas <daniel@scroll.com>
2020-03-28 10:26:32 -0400

    Extract supply info into log

--- scripts/build.sh
+++ scripts/build.sh
@@ -42,7 +42,7 @@ for f in campaigns/*; do
     count=1
 done
 echo ']' >> ./allCampaigns.json
-jq "[.[] | { campaignId: .campaign.id, section: map(.. .effects? | .[]? | select(.type==\"campaign_log\" and .text))  | group_by(.section) | .[] | { section: .[0].section, entries: map(. | { id: .id, text: .text })}}] | group_by(.campaignId) | map({ campaignId: .[0].campaignId, sections: map(.section) })" ./allCampaigns.json > campaignLogs.json
+jq "[.[] | { campaignId: .campaign.id, section: map(.. .effects? | .[]? | select(((.type==\"campaign_log\") or (.type==\"campaign_log_count\") or (.type==\"campaign_log_cards\")) and .text and (.cross_out != true))) | group_by(.section) | .[] | { section: .[0].section, entries: map(. | { id: .id, text: .text }) | unique}, supplies: map(.. .supplies? | .[]? ) | flatten }] | group_by(.campaignId) | map({ campaignId: .[0].campaignId, sections: map(.section), supplies: map(.supplies) | flatten | unique })" ./allCampaigns.json > campaignLogs.json
 cd ..
 cp build/allCampaigns.json demo/src/assets/allCampaigns.json
 cp build/campaignLogs.json demo/src/assets/campaignLogs.json
