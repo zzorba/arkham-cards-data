a81980874406c0e409fe68de41206cd724849bcc
Daniel Salinas <daniel@scroll.com>
2020-04-20 12:47:07 -0400

    Add tests for invalid encounter sets

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -11,6 +11,24 @@ if [[ $invalid_has_card ]]; then
   echo "A \"has_card\" condition has an invalid effect (only trauma allowed)"
 fi
 
+echo "Looking for invalid encounter sets"
+encounter_sets=$(jq ".[] | .scenarios[] | .steps[] | select(.type == \"encounter_sets\" and .encounter_sets != null) | .encounter_sets | .[]" build/allCampaigns.json)
+while IFS= read -r line; do
+  case `grep -F "$line" "../arkhamdb-json-data/encounters.json" >/dev/null; echo $?` in
+  0)
+    # code if found
+    ;;
+  1)
+    # code if not found
+    echo "Encounter code $line is invalid"
+    ;;
+  *)
+    # code if an error occurred
+    ;;
+  esac
+done <<< "$encounter_sets"
+echo "Done"
+
 scripts/generate.sh
 
 scripts/sync.sh
