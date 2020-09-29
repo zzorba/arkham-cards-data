961c0f486932e7b0b613dbb3bac4c71c4cde217f
Daniel Salinas <daniel@occamzrazor.com>
2020-09-29 09:55:09 -0500

    Add TIC translation placeholders

--- scripts/generateLocales.js
+++ scripts/generateLocales.js
@@ -171,10 +171,6 @@ async function generateLocale(localeCode) {
     if (scenario.indexOf(".DS_Store") !== -1) {
       continue;
     }
-    if (scenario.indexOf("/tic/") !== -1) {
-      // not ready for TIC yet
-      continue;
-    }
     const scenarioPoFile =
       "i18n/" + localeCode + "/" + scenario.replace(/json$/, "po");
     const poFile = await getOrCreatePOFile(scenarioPoFile, localeCode, scenario);
