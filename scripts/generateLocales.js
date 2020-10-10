b032a986a78d13c0f2ba5ab362bbdecaa85e607a
Daniel Salinas <daniel@occamzrazor.com>
2020-10-10 09:46:27 -0400

    Extract polish translation placeholders

--- scripts/generateLocales.js
+++ scripts/generateLocales.js
@@ -126,6 +126,10 @@ const SETTINGS_FOR_LANGUAGE = {
     'Language': 'ru',
     'Plural-Forms': 'nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : 2);',
   },
+  pl: {
+    'Language': 'pl',
+    'Plural-Forms': 'nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : 2);',
+  },
 };
 
 async function getOrCreatePOFile(scenarioPoFile, localeCode, scenario) {
