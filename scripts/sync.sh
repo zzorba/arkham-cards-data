6e1333a33fa0866c40d67456312abcdee6d6dfe6
Daniel Salinas <daniel@occamzrazor.com>
2021-12-16 16:03:56 -0600

    Move where generated files get synced to

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -13,23 +13,23 @@ fi
 
 if [ -d "$ARKHAM_CARDS" ]; then
   cp ./build/index.d.ts "$ARKHAM_CARDS/src/data/scenario/types.d.ts"
-  cp ./build/standalone.json "$ARKHAM_CARDS/assets/standaloneScenarios.json"
-  cp ./build/allCampaigns.json "$ARKHAM_CARDS/assets/allCampaigns.json"
-  cp ./build/campaignLogs.json "$ARKHAM_CARDS/assets/campaignLogs.json"
-  cp ./build/encounterSets.json "$ARKHAM_CARDS/assets/encounterSets.json"
-  cp ./errata/en/errata.json "$ARKHAM_CARDS/assets/campaignErrata.json"
-  cp ./taboos.json "$ARKHAM_CARDS/assets/taboos.json"
-  cp ./chaos_tokens.json "$ARKHAM_CARDS/assets/chaosOdds.json"
+  cp ./build/standalone.json "$ARKHAM_CARDS/assets/generated/standaloneScenarios.json"
+  cp ./build/allCampaigns.json "$ARKHAM_CARDS/assets/generated/allCampaigns.json"
+  cp ./build/campaignLogs.json "$ARKHAM_CARDS/assets/generated/campaignLogs.json"
+  cp ./build/encounterSets.json "$ARKHAM_CARDS/assets/generated/encounterSets.json"
+  cp ./errata/en/errata.json "$ARKHAM_CARDS/assets/generated/campaignErrata.json"
+  cp ./taboos.json "$ARKHAM_CARDS/assets/generated/taboos.json"
+  cp ./chaos_tokens.json "$ARKHAM_CARDS/assets/generated/chaosOdds.json"
 
   # I18N files
   LANGS=(es ko it fr ru de zh pt)
   for lang in ${LANGS[@]}; do
-    cp ./build/allCampaigns_$lang.json "$ARKHAM_CARDS/assets/allCampaigns_$lang.json"
-    cp ./build/campaignLogs_$lang.json "$ARKHAM_CARDS/assets/campaignLogs_$lang.json"
-    cp ./build/encounterSets_$lang.json "$ARKHAM_CARDS/assets/encounterSets_$lang.json"
-    cp ./errata/$lang/errata.json "$ARKHAM_CARDS/assets/campaignErrata_$lang.json"
-    cp ./build/chaos_tokens_$lang.json "$ARKHAM_CARDS/assets/chaosOdds_$lang.json"
-    cp ./build/i18n/$lang/taboos.json "$ARKHAM_CARDS/assets/taboos_$lang.json"
+    cp ./build/allCampaigns_$lang.json "$ARKHAM_CARDS/assets/generated/allCampaigns_$lang.json"
+    cp ./build/campaignLogs_$lang.json "$ARKHAM_CARDS/assets/generated/campaignLogs_$lang.json"
+    cp ./build/encounterSets_$lang.json "$ARKHAM_CARDS/assets/generated/encounterSets_$lang.json"
+    cp ./errata/$lang/errata.json "$ARKHAM_CARDS/assets/generated/campaignErrata_$lang.json"
+    cp ./build/chaos_tokens_$lang.json "$ARKHAM_CARDS/assets/generated/chaosOdds_$lang.json"
+    cp ./build/i18n/$lang/taboos.json "$ARKHAM_CARDS/assets/generated/taboos_$lang.json"
   done
 else
   echo "Folder $ARKHAM_CARDS does not exist."
