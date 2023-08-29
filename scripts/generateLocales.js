e55ea529bcebe0a467c036f7a3fd8c66d978e2af
Daniel Salinas <arkhamcards@gmail.com>
2023-08-29 19:29:26 -0400

    Update translations for FR

--- scripts/generateLocales.js
+++ scripts/generateLocales.js
@@ -82,7 +82,7 @@ async function writeJSON(object, filePath) {
   }
 }
 
-const TRANSLATEABLE_KEYS = new Set(['example', 'selected_text', 'selected_feminine_text', 'selected_nonbinary_text', 'masculine_text', 'feminine_text', 'nonbinary_text', 'text', 'confirmation_text', 'note', 'title', 'subtext', 'prompt', 'manual_prompt', 'header', 'name', 'description', 'confirm_text', 'scenario_name', 'full_name', 'linked_prompt']);
+const TRANSLATEABLE_KEYS = new Set(['example', 'selected_text', 'selected_feminine_text', 'selected_nonbinary_text', 'masculine_text', 'feminine_text', 'nonbinary_text', 'text', 'confirmation_text', 'note', 'title', 'subtext', 'prompt', 'manual_prompt', 'header', 'name', 'description', 'confirm_text', 'scenario_name', 'full_name', 'linked_prompt', 'file_name']);
 
 function translateField(object, prop, poFile, allPoEntries, corePoEntries, gender, starter) {
   const normalized = unorm.nfc(object[prop]);
